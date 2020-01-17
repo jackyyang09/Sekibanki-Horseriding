7048c9f99b4583dc951da60730c9ca1a0370a6f4
Jacky Yang <jackyyang267@gmail.com>
2020-01-16 19:35:48 -0500

    Check imported assets for lit tools

---
+++ Scripts/HorseManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HorseManager : MonoBehaviour
+{
+    /// <summary>
+    /// Speed at which the horse moves AKA distance point gain
+    /// </summary>
+    [SerializeField]
+    float speed;
+
+    public static HorseManager instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            // A unique case where the Singleton exists but not in this scene
+            if (instance.gameObject.scene.name == null)
+            {
+                instance = this;
+            }
+            else
+            {
+                Destroy(this);
+            }
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Scripts/UpgradeManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpgradeManager : MonoBehaviour
+{
+    public static UpgradeManager instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            // A unique case where the Singleton exists but not in this scene
+            if (instance.gameObject.scene.name == null)
+            {
+                instance = this;
+            }
+            else
+            {
+                Destroy(this);
+            }
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
